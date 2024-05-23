1d4dd6fe69079e01f2b026f8af0ff712bc08a862
Noor <noormmlk@gmail.com>
2024-05-23 13:40:08 -0600

    Created the form view in app.routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+from forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
@@ -15,4 +15,9 @@ def index():
         'body': 'The Avengers movie was so cool!'
     }]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Sign In', form=form)
 
