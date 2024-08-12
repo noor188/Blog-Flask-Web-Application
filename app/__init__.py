64ae7fd68f68ff1b7ff1ca6902e00307788ac180
Noor <noormmlk@gmail.com>
2024-08-12 12:00:30 -0600

    Database Migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # flask application instance
 from flask import Flask
-from config import Config
+from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
